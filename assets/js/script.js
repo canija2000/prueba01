87c7a1c6aa7b031c84b00fc0ae858fdd3040a3e7
Joaquin B <joaquin.bachler@gmail.com>
2024-01-24 22:44:48 -0300

    inclusion funcionalidades js,animaciones css, producto terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 const nav = document.getElementById('myNav');
 window.onscroll = function () { 
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
-        console.log("scroll");
+       
         nav.classList.add("nav-colored");
         nav.classList.remove("nav-transparent");
     } 
@@ -14,3 +14,42 @@ window.onscroll = function () {
         nav.classList.remove("nav-colored");
     }
 };
+
+// mensaje de confirmacion de envio de formulario
+// funcion para convertir la primera letra de un string en mayuscula
+function capitalizeFirstLetter(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+
+$(function(){
+    $("#contacto").click(function(){
+        let nombre = $("#name").val();
+        let asunto = $("#asunto").val();
+        let mensaje = $("#mensaje").val();
+        nombre = capitalizeFirstLetter(nombre.toLowerCase());
+        if(nombre == "" || asunto == "" || mensaje == ""){
+            alert("Por favor complete todos los campos");
+        }
+        else{
+            
+            alert(`Gracias ${nombre} por contactarnos, te responderemos a la brevedad`);
+        }
+
+    
+    });
+
+});
+
+// chequeo de campos vacios
+
+
+
+
+
+// tooltip de bootstrap 5
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
